dd72c785eb9fe8c1409a757afd1b37e404e3f811
Jason Whittle <jason.whittle@gmail.com>
2017-01-06 12:54:17 -0500

    Add support for embedding resources.

--- src/Data/Hal.hs
+++ src/Data/Hal.hs
@@ -8,15 +8,21 @@ module Data.Hal
   , Profile(..)
   , represent
   , linkTo
+  , embedSingle
+  , embedMulti
   ) where
 
 import GHC.Generics
 import Data.Aeson
 import Data.HashMap.Strict
+import Data.List (sortBy)
+import Data.Ord (comparing)
 import Data.Text (Text)
 
+type URI = Text
+
 data Link = Link
-  { href    :: Text
+  { href    :: URI
   , profile :: Maybe Text
   } deriving (Show, Generic)
 
@@ -29,25 +35,32 @@ data Representation = Representation
   { self :: Value
   , selfRel :: Link
   , links :: Links
+  , embeds :: Embeds
   } deriving (Show, Generic)
 
 class Profile a where
-  profileOf :: a -> Maybe Text
+  profileOf :: a -> Maybe URI
 
 instance ToJSON Representation where
-  toJSON = condenseLinks
+  toJSON = self . condenseEmbeds . condenseLinks
 
-condenseLinks :: Representation -> Value
-condenseLinks Representation{..} = case self of
-  Object o -> Object $ insert "_links" (toJSON links') o
+condenseEmbeds :: Representation -> Representation
+condenseEmbeds r@Representation{..} = case self of
+  Object o -> r { self = Object $ insert "_embedded" (toJSON embeds) o }
+  _ -> error "blargh"
+
+condenseLinks :: Representation -> Representation
+condenseLinks r@Representation{..} = case self of
+  Object o -> r { self = Object $ insert "_links" (toJSON links') o }
     where links' = addLink selfRel "self" links
   _ -> error "blargh"
 
-represent :: (Profile a, ToJSON a) => a -> Text -> Representation
+represent :: (Profile a, ToJSON a) => a -> URI -> Representation
 represent val uri = Representation
   { self = toJSON val
   , selfRel = Link uri $ profileOf val
   , links = empty
+  , embeds = empty
   }
 
 linkTo :: Link -> Text -> Representation -> Representation
@@ -55,3 +68,25 @@ linkTo l rel rep = rep { links = addLink l rel $ links rep }
 
 addLink :: Link -> Text -> Links -> Links
 addLink l rel ls = insert rel l ls
+
+
+type Embeds = HashMap Text EmbedGroup
+
+data EmbedGroup
+  = SingletonEmbed Representation
+  | EmbedArray (HashMap URI Representation)
+  deriving (Show)
+
+instance ToJSON EmbedGroup where
+  toJSON (SingletonEmbed r) = toJSON r
+  toJSON (EmbedArray m) = toJSON $ fmap snd $ sortBy (comparing fst) $ toList m
+
+embedSingle :: Text -> Representation -> Representation -> Representation
+embedSingle label a rep = rep { embeds = embeds' }
+  where embeds' = insert label (SingletonEmbed a) $ embeds rep
+
+embedMulti :: Text -> Representation -> Representation -> Representation
+embedMulti label a rep = rep { embeds = alter f label $ embeds rep }
+  where f Nothing = Just $ EmbedArray $ singleton (href $ selfRel a) a
+        f (Just (EmbedArray m)) = Just $ EmbedArray $ insert (href $ selfRel a) a m
+        f (Just (SingletonEmbed _)) = error "blargh"
--- test/Data/HalSpec.hs
+++ test/Data/HalSpec.hs
@@ -44,6 +44,57 @@ spec = do
     it "encodes the added link at the given rel in links" $ do
       pointTo "/_links/icon/href" (toJSON rep) `shouldBe` Right "http://static.test/images/ex/1"
       pointTo "/_links/icon/profile" (toJSON rep) `shouldBe` Right "http://foo.doc/types/icon"
+  describe "object with one singleton embed added" $ do
+    let ex = Ex { foo = "baz", bar = 42 }
+        embedded = Ex { foo = "quux", bar = 3 }
+        rep = embedSingle "ex" (represent embedded "http://foo.test/ex/2")
+              $ represent ex "http://foo.test/ex/1"
+    it "encodes the basic state at the root" $ do
+      pointTo "/foo" (toJSON rep) `shouldBe` Right "baz"
+      pointTo "/bar" (toJSON rep) `shouldBe` Right (Number 42)
+    it "encodes a link to self at the self rel in links" $ do
+      pointTo "/_links/self/href" (toJSON rep) `shouldBe` Right "http://foo.test/ex/1"
+    it "gives the object’s profile in the self rel" $ do
+      pointTo "/_links/self/profile" (toJSON rep) `shouldBe` Right "http://foo.doc/types/ex"
+    it "embeds the additional object" $ do
+      pointTo "/_embedded/ex/foo" (toJSON rep) `shouldBe` Right "quux"
+      pointTo "/_embedded/ex/bar" (toJSON rep) `shouldBe` Right (Number 3)
+    it "includes the self rel for the embedded object" $ do
+      pointTo "/_embedded/ex/_links/self/href" (toJSON rep) `shouldBe` Right "http://foo.test/ex/2"
+  describe "object with one embed in an array" $ do
+    let ex = Ex { foo = "baz", bar = 42 }
+        embedded = Ex { foo = "quux", bar = 3 }
+        rep = embedMulti "xs" (represent embedded "http://foo.test/ex/2")
+              $ represent ex "http://foo.test/ex/1"
+    it "encodes the basic state at the root" $ do
+      pointTo "/foo" (toJSON rep) `shouldBe` Right "baz"
+      pointTo "/bar" (toJSON rep) `shouldBe` Right (Number 42)
+    it "encodes a link to self at the self rel in links" $ do
+      pointTo "/_links/self/href" (toJSON rep) `shouldBe` Right "http://foo.test/ex/1"
+    it "gives the object’s profile in the self rel" $ do
+      pointTo "/_links/self/profile" (toJSON rep) `shouldBe` Right "http://foo.doc/types/ex"
+    it "embeds the additional object in an array" $ do
+      pointTo "/_embedded/xs/0/foo" (toJSON rep) `shouldBe` Right "quux"
+      pointTo "/_embedded/xs/0/bar" (toJSON rep) `shouldBe` Right (Number 3)
+    it "includes the self rel for the embedded object" $ do
+      pointTo "/_embedded/xs/0/_links/self/href" (toJSON rep) `shouldBe` Right "http://foo.test/ex/2"
+  describe "object with two embeds in the same array" $ do
+    let ex1 = Ex { foo = "foo", bar = 1 }
+        ex2 = Ex { foo = "bar", bar = 2 }
+        ex3 = Ex { foo = "baz", bar = 3 }
+        rep = embedMulti "xs" (represent ex3 "http://foo.test/ex/3")
+              $ embedMulti "xs" (represent ex2 "http://foo.test/ex/2")
+              $ represent ex1 "http://foo.test/ex/1"
+    it "encodes the basic state at the root" $ do
+      pointTo "/foo" (toJSON rep) `shouldBe` Right "foo"
+      pointTo "/bar" (toJSON rep) `shouldBe` Right (Number 1)
+    it "encodes a link to self at the self rel in links" $ do
+      pointTo "/_links/self/href" (toJSON rep) `shouldBe` Right "http://foo.test/ex/1"
+    it "gives the object’s profile in the self rel" $ do
+      pointTo "/_links/self/profile" (toJSON rep) `shouldBe` Right "http://foo.doc/types/ex"
+    it "embeds both additional objects in the same array" $ do
+      pointTo "/_embedded/xs/0/foo" (toJSON rep) `shouldBe` Right "bar"
+      pointTo "/_embedded/xs/1/foo" (toJSON rep) `shouldBe` Right "baz"
 
 toPtr :: Text -> Pointer
 toPtr t = let Right p = unescape t in p
